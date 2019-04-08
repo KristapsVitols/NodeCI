c17caeb9f9367e04b626f5cf419bfbef1c0c3f40
kristapsv <kristapsvitols91@gmail.com>
2019-04-09 00:04:40 +0300

    red

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID:
         '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
     googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-    mongoURI: 'mongodb://admin:password123@ds251804.mlab.com:51804/blog_devx',
+    mongoURI: 'mongodb://acc:pass@ds251804.mlab.com:51804/blog_devx',
     cookieKey: '123123123',
     redisUrl: 'redis://127.0.0.1:6379'
 };
